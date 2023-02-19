1c756be5823b1e77d5958937bcf193d3c725c7e6
gyoenge <gyoeng94@gmail.com>
2023-02-19 19:11:03 +0900

    service page 링크 버튼 수정

--- src/data/serviceInfos.js
+++ src/data/serviceInfos.js
@@ -19,7 +19,7 @@ export const serviceInfoList = [
     info: "AMS는 학생자치회 및 동아리의 선발 과정을 돕는 지원 관리 시스템입니다. 지원자 정보와 지원서, 면접 일정을 통합적으로 관리하고 편리하게 지원자와 연락할 수 있도록 해 줍니다. ",
     imgSrc: amsImg,
     logoSrc: amsLogo,
-    pageLink: "https://gistory.me/",
+    pageLink: "",
     githubLink: "",
   },
   {
@@ -27,7 +27,7 @@ export const serviceInfoList = [
     info: "GISTALK은 학생들이 GIST에서 수강한 수업을 평가하고 공유하는 사이트입니다. 강의에 대한 정량적인 평가를 직관적인 육각형 레이더 차트로 제공하며, 포인트 제도로 학생들이 강의평가를 쓰도록 장려하고 있습니다. ",
     imgSrc: gistalkImg,
     logoSrc: gistalkLogo,
-    pageLink: "https://gistory.me/",
-    githubLink: "https://github.com/",
+    pageLink: "https://gistalk.gistory.me/",
+    githubLink: "",
   },
 ];
